b493e0e5b7825e927c3c69082aed46c0c8629581
nvFathima <fathimanv2004@gmail.com>
2025-07-16 21:07:57 +0530

    Db connect updation

--- DB_connection.php
+++ DB_connection.php
@@ -1,9 +1,9 @@
 <?php  
 
-$sName = "db";
-$uName = "root";
-$pass  = "root";
-$db_name = "task_management";
+$sName = "dpg-d1rsd96uk2gs73b2j7ug-a";
+$uName = "task_management_z0z2_user";
+$pass  = "DP5WvJQwiiFD9XhklxoH8l7TCaixCx82";
+$db_name = "task_management_z0z2";
 
 try {
 	$conn = new PDO("mysql:host=$sName;dbname=$db_name", $uName, $pass);
